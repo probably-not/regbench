526c5e4b634ef3b37b03dd3fdc3ee3664a48fad7
Coby Benveniste <coby.benveniste@gmail.com>
2024-02-28 15:14:06 -0500

    Add PG based registry

---
+++ lib/regbench/registries/pg.ex
@@ -0,0 +1,29 @@
+defmodule Regbench.Registries.PG do
+  @behaviour Regbench.Benchmark
+
+  def init() do
+    :pg.start_link()
+  end
+
+  def register(key, pid) do
+    :ok = :pg.join(key, pid)
+  end
+
+  def unregister(key, pid) do
+    :ok = :pg.leave(key, pid)
+  end
+
+  def retrieve(key) do
+    case :pg.get_members(key) do
+      [] -> :undefined
+      [pid] -> pid
+    end
+  end
+
+  def process_loop() do
+    receive do
+      _ ->
+        :ok
+    end
+  end
+end
