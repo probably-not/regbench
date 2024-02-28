bef5251f6509a1cd589b5a8261fee62703b2ab3d
Coby Benveniste <coby.benveniste@gmail.com>
2024-02-28 14:30:25 -0500

    Add global registry benchmark

---
+++ lib/regbench/registries/global.ex
@@ -0,0 +1,26 @@
+defmodule Regbench.Registries.Global do
+  @behaviour Regbench.Benchmark
+
+  def init() do
+    :ok
+  end
+
+  def register(key, pid) do
+    :yes = :global.register_name(key, pid)
+  end
+
+  def unregister(key, _pid) do
+    :global.unregister_name(key)
+  end
+
+  def retrieve(key) do
+    :global.whereis_name(key)
+  end
+
+  def process_loop() do
+    receive do
+      _ ->
+        :ok
+    end
+  end
+end
