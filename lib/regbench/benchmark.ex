5169e62701a5f24f5807b435e96894341336ecca
Coby Benveniste <coby.benveniste@gmail.com>
2024-02-28 13:35:54 -0500

    Add benchmark behaviour

---
+++ lib/regbench/benchmark.ex
@@ -0,0 +1,11 @@
+defmodule Regbench.Benchmark do
+  @moduledoc """
+  A behaviour for a Process Registry benchmark.
+  """
+
+  @callback init() :: term()
+  @callback register(key :: String.t(), pid()) :: term()
+  @callback unregister(key :: String.t(), pid()) :: term()
+  @callback retrieve(key :: String.t()) :: pid() | nil
+  @callback process_loop() :: any()
+end
