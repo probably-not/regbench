5f00e2ae606970a23e425946e19405a20eec991d
Coby Benveniste <coby.benveniste@gmail.com>
2024-03-31 08:36:50 +0300

    Make registration more Elixir-y

--- lib/regbench/phases/registration.ex
+++ lib/regbench/phases/registration.ex
@@ -11,27 +11,31 @@ defmodule Regbench.Phases.Registration do
   @doc false
   @spec run(
           benchmark_mod :: Regbench.Benchmark.t(),
-          node_pid_infos :: Regbench.Phases.Launch.node_pid_infos()
+          node_pid_infos :: list(Regbench.Phases.Launch.node_pid_infos())
         ) :: :ok
   def run(benchmark_mod, nodes_pid_infos) do
-    Enum.reduce(nodes_pid_infos, [], fn {node, node_pid_infos}, acc ->
-      rpc_key =
-        :rpc.async_call(node, __MODULE__, :register_on_node, [benchmark_mod, node_pid_infos])
+    nodes_pid_infos
+    |> Task.async_stream(
+      fn {node, node_pid_infos} ->
+        tpid = self()
 
-      [{node, rpc_key} | acc]
-    end)
-    |> Enum.each(fn {node, rpc_key} ->
-      registered = :rpc.yield(rpc_key)
-      IO.puts("Registered #{registered} processes on node #{node}")
-    end)
-  end
+        Node.spawn(node, fn ->
+          for {key, npid} <- node_pid_infos do
+            benchmark_mod.register(key, npid)
+          end
 
-  def register_on_node(benchmark_mod, node_pid_infos) do
-    node_pid_infos
-    |> Enum.each(fn {key, pid} ->
-      benchmark_mod.register(key, pid)
-    end)
+          send(tpid, {:registered, length(node_pid_infos)})
+        end)
 
-    length(node_pid_infos)
+        receive do
+          {:registered, count} ->
+            IO.puts("Registered #{count} processes on node #{node}")
+        end
+
+        nil
+      end,
+      ordered: false
+    )
+    |> Stream.run()
   end
 end
