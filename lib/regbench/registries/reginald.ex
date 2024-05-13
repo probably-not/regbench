ca9abb0d77c6b04f2fb224eb39ea8bf2c0ea29d8
Coby Benveniste <coby.benveniste@gmail.com>
2024-05-13 16:13:39 +0300

    Update Reginald to use the registry for underlying functionality

--- lib/regbench/registries/reginald.ex
+++ lib/regbench/registries/reginald.ex
@@ -12,14 +12,45 @@ defmodule Regbench.Registries.Reginald do
   end
 
   def register(key, pid) do
-    Reginald.register_name({__MODULE__, key}, pid)
+    ref = make_ref()
+    send(pid, {:register, key, ref, self()})
+
+    receive do
+      {:registered, :yes, ^ref} -> :yes
+    after
+      5_000 -> throw("unexpected registration timeout")
+    end
   end
 
-  def deregister(key, _pid) do
-    :ok = Reginald.unregister_name({__MODULE__, key})
+  def deregister(key, pid) do
+    ref = make_ref()
+    send(pid, {:deregister, key, ref, self()})
+
+    receive do
+      {:deregistered, :ok, ^ref} -> :ok
+    after
+      5_000 -> throw("unexpected registration timeout")
+    end
   end
 
   def retrieve(key) do
     Reginald.whereis_name({__MODULE__, key})
+  end
+
+  def process_loop() do
+    receive do
+      {:register, name, ref, origin} ->
+        result = Reginald.register_name({__MODULE__, name}, self())
+        send(origin, {:registered, result, ref})
+        process_loop()
+
+      {:deregister, name, ref, origin} ->
+        result = Reginald.unregister_name({__MODULE__, name})
+        send(origin, {:deregistered, result, ref})
+        process_loop()
+
+      _ ->
+        :ok
+    end
   end
 end
